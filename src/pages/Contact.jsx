6078821b1d8eeb0f0ebdf5f50243d9b176687118
Ghaith A <alghaith.ah38@gmail.com>
2024-07-29 13:28:15 -0400

    Cleaned up project files and began making it into my portfolio.

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,10 @@
+const Contact = () => {
+    return (
+        <div>
+            <h1> Get in touch with me! </h1>
+            <p> E-mail: alghaith.ah38@gmail.com </p>
+        </div>
+    )
+}
+
+export default Contact
---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+const Home = () => {
+    return (
+        <>
+            <h1>Alghaith Ahmad - Software Engineer</h1>
+            <div className="card">
+                <h2>Who am I?</h2>
+                <p>Answer</p>
+            </div>
+            <div className="card">
+                <h2>Who am I?</h2>
+                <p>Answer</p>
+            </div>
+        </>
+    )
+}
+
+export default Home
